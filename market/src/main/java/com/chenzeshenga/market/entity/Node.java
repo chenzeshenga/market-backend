53cfd0fd47557de3927d685ec68c30596b3a7cc0
chenzeshenga <chenzeshenga@163.com>
2018-04-22 00:06:42 +0800

    add classify controller

---
+++ market/src/main/java/com/chenzeshenga/market/entity/Node.java
@@ -0,0 +1,24 @@
+package com.chenzeshenga.market.entity;
+
+public class Node {
+
+	private int id;
+	private Classify tree;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public Classify getTree() {
+		return tree;
+	}
+
+	public void setTree(Classify tree) {
+		this.tree = tree;
+	}
+
+}
