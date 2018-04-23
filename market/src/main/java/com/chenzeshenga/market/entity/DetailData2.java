cc15aa426042505dba465e9c4f67fcf62e60cb57
chenzeshenga <chenzeshenga@163.com>
2018-04-23 08:33:21 +0800

    image controller unimpl

---
+++ market/src/main/java/com/chenzeshenga/market/entity/DetailData2.java
@@ -0,0 +1,35 @@
+package com.chenzeshenga.market.entity;
+
+import java.util.List;
+
+public class DetailData2 {
+
+	private int code;
+	private String msg;
+	private List<Details> data;
+
+	public int getCode() {
+		return code;
+	}
+
+	public void setCode(int code) {
+		this.code = code;
+	}
+
+	public String getMsg() {
+		return msg;
+	}
+
+	public void setMsg(String msg) {
+		this.msg = msg;
+	}
+
+	public List<Details> getData() {
+		return data;
+	}
+
+	public void setData(List<Details> data) {
+		this.data = data;
+	}
+
+}
